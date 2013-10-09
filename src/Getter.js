ffc3e07f4d498b9a4d877dc7ecb8686bf1a30ac5
EragonJ <eragonj@eragonj.me>
2013-10-10 01:51:43 +0800

    add ie support for xhr

--- src/Getter.js
+++ src/Getter.js
@@ -8,24 +8,17 @@
 
 (function() {
 
-    // TODO
-    // check arguments later
     var Getter = function(url, callback) {
-        var xhr;
-
-        if (typeof XMLHttpRequest !== "undefined") {
-            xhr = new XMLHttpRequest(); 
-        }
-        else {
-            // No IE here
-        }
+        var xhr = getXHR();
 
         xhr.onreadystatechange = function() {
             if (xhr.readyState < 4) {
                 return;
             }
 
+            // TODO: We can do more about logging later
             if (xhr.status !== 200) {
+                throw new Error(xhr.statusText);
                 return;
             }
 
@@ -37,6 +30,24 @@
         xhr.open("GET", url, true);
         xhr.send("");
     };
+
+    /*
+     * http://goo.gl/5NkoJS
+     */
+    function getXHR() {
+
+        if (window.XMLHttpRequest) {
+            return new XMLHttpRequest(); 
+        }
+
+        try { return new ActiveXObject("Msxml2.XMLHTTP.6.0"); }
+        catch (e) {}
+        try { return new ActiveXObject("Msxml2.XMLHTTP.3.0"); }
+        catch (e) {}
+        try { return new ActiveXObject("Microsoft.XMLHTTP"); }
+        catch (e) {}
+        throw new Error("This browser does not support XMLHttpRequest.");
+    }
 
     Completed.module.add("Getter", Getter);
 }());
